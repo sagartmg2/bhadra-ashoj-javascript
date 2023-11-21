90f5f9a1728f76562fa1ee0a8ca806a9a0a0f378
Sagar <dev.sagartmg@gmail.com>
2023-11-21 16:59:31 +0545

    operators and if-else

---
+++ 8-operator.js
@@ -0,0 +1,50 @@
+/* 
+    operators
+        assignment 
+        arthematic opertor
+            + - ....
+            %  modolous ..gives out remainder
+            / division
+            * multiplicatio
+        increment   
+            post increment
+                eg  
+                    let num = 2
+                    num ++
+            pre increment
+            eg  
+                    let num = 2
+                    ++ num 
+        
+
+        comparision operators
+        == equality opertor
+        === strict equality operator
+        >
+        >=
+        <
+        <=
+
+        logical operators
+            AND
+            OR
+            NOT
+
+*/
+
+let title = "web"
+
+console.log(10 % 3)
+
+let number = 100
+number++
+console.log(number);
+
+
+let titleOne = "web"
+let titleTwo = "Web"
+console.log(titleOne == titleTwo);
+
+
+console.log(1 == "1"); // true
+console.log(1 === "1"); // false
